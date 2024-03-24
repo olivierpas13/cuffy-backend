22352198661677045df0434f5d5a70d107462c73
Olivier Paspuel <olivier_paspuel@hotmail.com>
2024-03-24 08:40:42 -0500

    fix: vercel deployment requires different structure

--- api/index.js
+++ api/index.js
@@ -1,13 +1,63 @@
-import http from 'http';
-import app from '../app.js';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { MONGODB_URI, PORT, SECRET } from "../utils/config.js";
+import { auth } from "express-openid-connect";
+import morgan from "morgan";
+import pkg from 'express-openid-connect';
+// import loginRouter from "./controllers/login.js";
+// import userRouter from "./controllers/user.js";
+import productsRouter from "../controllers/products.js";
 
-const server = http.createServer(app);
+const app = express();
 
-const PORT = process.env.PORT || 3003;
+console.log("connecting to", MONGODB_URI);
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB", error.message);
+  });
+
+  const baseUrl = process.env.NODE_ENV === 'development'
+  ? `http://localhost:3003`
+  : "https://cuffy-backend.vercel.app";
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: SECRET,
+  baseURL: baseUrl,
+  clientID: "DhP0NihlK7e1JRZcFPUyF0nb4EaqDWEo",
+  issuerBaseURL: 'https://dev-vf1leamsnvsgn37c.us.auth0.com'
+};
+
+app.use(auth(config));
+
+app.use(cors());
+
+app.use(morgan('dev'))
+
+app.use(express.static("./.next"));
+
+app.use(express.json());
+
+// app.use(tokenExtractor);
+
+app.get('/', (req, res) => {
+    res.json("Cuffy Backend")
 });
 
-// const server = app.listen(
-//   PORT, () => console.log(`API Server listening on port ${PORT}`));
+
+app.get('/profile', pkg.requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+
+app.use("/api/products", productsRouter);
+
+export default app;
---
+++ api/server.js
@@ -0,0 +1,13 @@
+import http from 'http';
+import app from './index.js';
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3003;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// const server = app.listen(
+//   PORT, () => console.log(`API Server listening on port ${PORT}`));
