66522e56304c1ff635523630f42b5896d343d783
Olivier Paspuel <olivier_paspuel@hotmail.com>
2024-03-24 10:26:38 -0500

    fix: vercel.json and structure changed to deploy on vercel

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import { MONGODB_URI, PORT, SECRET } from "../utils/config.js";
-import { auth } from "express-openid-connect";
+import {MONGODB_URI} from './utils/config.js'
+// import { auth } from "express-openid-connect";
 import morgan from "morgan";
-import pkg from 'express-openid-connect';
-// import loginRouter from "./controllers/login.js";
-// import userRouter from "./controllers/user.js";
-import productsRouter from "../controllers/products.js";
+// import pkg from 'express-openid-connect';
+import productsRouter from "./controllers/products.js";
 
 const app = express();
+
+const PORT = process.env.PORT || 3003;
 
 console.log("connecting to", MONGODB_URI);
 
@@ -22,27 +22,27 @@ mongoose
     console.error("Error connecting to MongoDB", error.message);
   });
 
-  const baseUrl = process.env.NODE_ENV === 'development'
-  ? `http://localhost:3003`
-  : "https://cuffy-backend.vercel.app";
+  // const baseUrl = process.env.NODE_ENV === 'development'
+  // ? `http://localhost:3003`
+  // : "https://cuffy-backend.vercel.app";
 
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: SECRET,
-  baseURL: baseUrl,
-  clientID: "DhP0NihlK7e1JRZcFPUyF0nb4EaqDWEo",
-  issuerBaseURL: 'https://dev-vf1leamsnvsgn37c.us.auth0.com'
-};
+// const config = {
+//   authRequired: false,
+//   auth0Logout: true,
+//   secret: SECRET,
+//   baseURL: baseUrl,
+//   clientID: "DhP0NihlK7e1JRZcFPUyF0nb4EaqDWEo",
+//   issuerBaseURL: 'https://dev-vf1leamsnvsgn37c.us.auth0.com'
+// };
 
-app.use(auth(config));
+// app.use(auth(config));
 
 app.use(cors());
 
 app.use(morgan('dev'))
 
-app.use(express.static("./.next"));
+// app.use(express.static("./.next"));
 
 app.use(express.json());
 
@@ -53,11 +53,13 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/profile', pkg.requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
+// app.get('/profile', pkg.requiresAuth(), (req, res) => {
+//   res.send(JSON.stringify(req.oidc.user));
+// });
 
 
 app.use("/api/products", productsRouter);
+
+app.listen(PORT, ()=> console.log("Server ready at port 3003"));
 
 export default app;
