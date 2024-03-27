8da91009126a133c3a7a946b695cf0da0a2b96aa
Olivier Paspuel <olivier_paspuel@hotmail.com>
2024-03-27 15:39:50 -0500

    fix: nextpage response fixed

--- api/services/productService.js
+++ api/services/productService.js
@@ -39,7 +39,7 @@ class productService {
       const res = {
         page,
         currentPage: Number(index),
-        nextPage: Number(index + PAGE_SIZE) <  this.totalProds ? Number(index + PAGE_SIZE) : null,
+        nextPage: Number(index) <  this.totalProds ? Number(index + PAGE_SIZE) : null,
       }
       return res;
     } catch (error) {
